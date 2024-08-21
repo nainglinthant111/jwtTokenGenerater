1065c6d3f0ae4451a151c14be0a197ecb408a31b
nainglinthant <oppositedirection111@gmail.com>
2024-08-21 19:46:49 +0630

    SecretKey Update

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
-//(Base64-encoded)
-const secretKey = 'u5e1DZB5+/7blHgN4lPIRg==';
-// const secretKey = process.env.SECRET_KEY;
-
-async function generateToken(jsonPayload) {
+async function generateToken(secretKey,jsonPayload) {
     const key = await importJWK({
         kty: 'oct',
         k: secretKey,
@@ -32,12 +28,17 @@ app.get('/',async(req,res)=>{
 });
 app.post('/generate-token', async (req, res) => {
     try {
-        const jsonPayload = req.body;
-        const token = await generateToken(jsonPayload); 
-        console.log(token);
-        res.status(200).json({
-            token: token
-        });
+        const secretKey = req.headers['x-secret-key'];
+        if(secretKey){
+            const jsonPayload = req.body;
+            const token = await generateToken(secretKey,jsonPayload); 
+            console.log("Token Ok!");
+            res.status(200).json({
+                token: token
+            });
+        }else{
+            res.status(404).send('SecretKey key is error');
+        }
     } catch (error) {
         console.error('Error generating token:', error);
         res.status(500).send('Internal Server Error');
